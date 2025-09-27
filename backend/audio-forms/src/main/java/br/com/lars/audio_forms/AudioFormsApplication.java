08208ad449352b8180cc1a1d4867f9fb206f776e
mthxz <matheus.sinto@unesp.br>
2025-09-27 11:03:01 -0300

    starting project

---
+++ backend/audio-forms/src/main/java/br/com/lars/audio_forms/AudioFormsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lars.audio_forms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioFormsApplication.class, args);
+	}
+
+}
---
+++ backend/audio-forms/src/test/java/br/com/lars/audio_forms/AudioFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lars.audio_forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
